ca7566acdff808f8215510e204eaf5807eab94f4
Zekaroni <ezekiel.hall.04@gmail.com>
2024-05-20 12:15:18 -0400

    updated GraphingAPI

--- SunSpecGraphing/GraphingAPI.cs
+++ SunSpecGraphing/GraphingAPI.cs
@@ -20,12 +20,16 @@ namespace SunSpecGraphing
         //  ones are more like "settings")
         public bool useGraphingColor = false;
 
-        private int GRAPH_PADDING_TOP    = 3;
-        private int GRAPH_PADDING_BOTTOM = 3;
+        private int GRAPH_PADDING_TOP    = 0;
+        private int GRAPH_PADDING_BOTTOM = 0;
+        private int GRAPH_PADDING_LEFT   = 0;
+        private int GRAPH_PADDING_RIGHT  = 0;
         private int SPECTRA_BAR_HEIGHT   = 50;
 
         // This will be changable based on the users option
         private int MAX_INTENSITY_VALUE = 65535;
+        private string GraphFontType = "Arial";
+        private int GraphFontSize = 10;
 
 
         public int[] wavelengthPositions = new int[Global.SAMPLE_SIZE];
@@ -48,6 +52,90 @@ namespace SunSpecGraphing
             Init();
         }
 
+        private void DrawAxes(Graphics g)
+        {
+            using (Pen axisPen = new Pen(Color.Black, 2))
+            {
+                // X-Axis
+                g.DrawLine(axisPen,
+                    POS_X + GRAPH_PADDING_LEFT,
+                    HEIGHT - GRAPH_PADDING_BOTTOM + POS_Y,
+                    WIDTH - GRAPH_PADDING_RIGHT + POS_X,
+                    HEIGHT - GRAPH_PADDING_BOTTOM + POS_Y
+                );
+
+                // Y-Axis
+                g.DrawLine(axisPen,
+                    POS_X + GRAPH_PADDING_LEFT,
+                    POS_Y + GRAPH_PADDING_TOP,
+                    POS_X + GRAPH_PADDING_LEFT,
+                    HEIGHT - GRAPH_PADDING_BOTTOM + POS_Y
+                );
+            }
+        }
+
+        private void DrawLabels(Graphics g)
+        {
+            int labelStep = 100;
+            using (Font font = new Font("Arial", 10))
+            using (Brush brush = new SolidBrush(Color.Black))
+            {
+                // Draw X axis labels
+                for (int i = 0; i < Global.SAMPLE_SIZE; i += labelStep)
+                {
+                    string label = Math.Round(Global.WAVELENGTHS[i]).ToString();
+                    SizeF labelSize = g.MeasureString(label, font);
+                    float x = wavelengthPositions[i] - labelSize.Width / 2;
+                    float y = HEIGHT - GRAPH_PADDING_BOTTOM + POS_Y + 5;
+                    g.DrawString(label, font, brush, x, y);
+                }
+
+                // Draw Y-Axis labels (e.g., intensity values)
+                int numberOfLabels = 10;
+                for (int i = 0; i <= numberOfLabels; i++)
+                {
+                    int intensity = MAX_INTENSITY_VALUE / numberOfLabels * i;
+                    string label = intensity.ToString();
+                    SizeF labelSize = g.MeasureString(label, font);
+                    float x = POS_X + GRAPH_PADDING_LEFT - labelSize.Width - 5; // Adjusted for padding
+                    float y = CalculateYPosition(intensity) - labelSize.Height / 2;
+                    g.DrawString(label, font, brush, x, y);
+                }
+            }
+        }
+
+        private void DrawLegend(Graphics g)
+        {
+            using (Font font = new Font(GraphFontType, GraphFontSize))
+            using (Brush brush = new SolidBrush(Color.Black))
+            {
+                string legend = "Wavelength Intensities";
+                SizeF legendSize = g.MeasureString(legend, font);
+                float x = WIDTH + POS_X - GRAPH_PADDING_RIGHT - legendSize.Width - 10;
+                float y = POS_Y + GRAPH_PADDING_TOP;
+                g.DrawString(legend, font, brush, x, y);
+            }
+        }
+
+        private int CalculateYPosition(int Intensity)
+        {
+            int adjustedHeight = HEIGHT - GRAPH_PADDING_TOP - GRAPH_PADDING_BOTTOM;
+            int y = (int)(((double)Intensity / (double)MAX_INTENSITY_VALUE) * (double)adjustedHeight);
+            y = HEIGHT - y - GRAPH_PADDING_BOTTOM + POS_Y;
+            return y;
+        }
+
+        private void BakeWavelengthPositions()
+        {
+            int adjustedWidth = WIDTH - GRAPH_PADDING_LEFT - GRAPH_PADDING_RIGHT;
+            double wavelengthStart = Global.WAVELENGTHS[0];
+
+            for (int i = 0; i < Global.SAMPLE_SIZE; i++)
+            {
+                wavelengthPositions[i] = (int)((double)(Global.WAVELENGTHS[i] - wavelengthStart) / (double)Global.SPECTROMETER_RANGE * (double)adjustedWidth) + GRAPH_PADDING_LEFT + POS_X;
+            }
+        }
+
         private void DrawDataGraph()
         {
             Point[] points = new Point[Global.SAMPLE_SIZE];
@@ -58,6 +146,23 @@ namespace SunSpecGraphing
 
             using (Graphics g = parent.CreateGraphics())
             {
+                // Clear the drawing area
+                g.Clear(parent.BackColor);
+
+                // Fill the area behind the graph with white
+                using (Brush whiteBrush = new SolidBrush(Color.Gray))
+                {
+                    g.FillRectangle(whiteBrush,
+                        POS_X + GRAPH_PADDING_LEFT,
+                        POS_Y + GRAPH_PADDING_TOP,
+                        WIDTH - GRAPH_PADDING_LEFT - GRAPH_PADDING_RIGHT,
+                        HEIGHT - GRAPH_PADDING_TOP - GRAPH_PADDING_BOTTOM);
+                }
+
+                // Draw axes
+                DrawAxes(g);
+
+                // Draw data points
                 if (useGraphingColor)
                 {
                     ColorBlend colorBlend = new ColorBlend();
@@ -77,8 +182,13 @@ namespace SunSpecGraphing
                         g.DrawLines(pen, points);
                     }
                 }
+
+                // Draw legend and labels
+                DrawLegend(g);
+                DrawLabels(g);
             }
         }
+
 
         private void DrawSpectraBar()
         {
@@ -86,54 +196,39 @@ namespace SunSpecGraphing
 
             using (Graphics g = parent.CreateGraphics())
             {
-                Rectangle rect = new Rectangle(
-                    POS_X,                                 // X
-                    (HEIGHT - SPECTRA_BAR_HEIGHT) + POS_Y, // Y
-                    WIDTH,                                 // Width
-                    SPECTRA_BAR_HEIGHT                     // Height
-                );
-
-
-                // Creates a solid black bacground which help defines the spectra
-                // TODO: Maybe add an option for the background colour??
-                using (SolidBrush backgroundBrush = new SolidBrush(Color.Black))
+                using (Font font = new Font(GraphFontType, GraphFontSize))
                 {
-                    g.FillRectangle(backgroundBrush, rect);
-                }
+                    SizeF labelSize = g.MeasureString("Example", font);
+                    int labelHeight = (int)labelSize.Height;
+                    int spectraBarYPosition = POS_Y + HEIGHT - GRAPH_PADDING_BOTTOM + labelHeight + 10;
 
-                // Create the blend of spectra based on the active alpha/intensity of data
-                ColorBlend colorBlend = new ColorBlend();
-                colorBlend.Colors = ActiveWavelengthColorIntensities;
-                colorBlend.Positions = WavelenghtGradientPositions;
+                    Rectangle rect = new Rectangle(
+                        POS_X,                      // X
+                        spectraBarYPosition,        // Y
+                        WIDTH,                      // Width
+                        SPECTRA_BAR_HEIGHT          // Height
+                    );
 
-                using (LinearGradientBrush brush = new LinearGradientBrush(rect, Color.Red, Color.Blue, LinearGradientMode.Horizontal))
-                {
-                    brush.InterpolationColors = colorBlend;
-                    g.FillRectangle(brush, rect);
+                    // Creates a solid black background which helps define the spectra
+                    using (SolidBrush backgroundBrush = new SolidBrush(Color.Black))
+                    {
+                        g.FillRectangle(backgroundBrush, rect);
+                    }
+
+                    // Create the blend of spectra based on the active alpha/intensity of data
+                    ColorBlend colorBlend = new ColorBlend();
+                    colorBlend.Colors = ActiveWavelengthColorIntensities;
+                    colorBlend.Positions = WavelenghtGradientPositions;
+
+                    using (LinearGradientBrush brush = new LinearGradientBrush(rect, Color.Red, Color.Blue, LinearGradientMode.Horizontal))
+                    {
+                        brush.InterpolationColors = colorBlend;
+                        g.FillRectangle(brush, rect);
+                    }
                 }
             }
         }
 
-        private int CalculateYPosition(int Intensity)
-        {
-            // Calculate Y
-
-            // TODO: Add logic for if local or global max
-
-            int y = (HEIGHT - (int)(((double)Intensity / (double)MAX_INTENSITY_VALUE) * (double)HEIGHT)) + POS_Y;
-            // Console.WriteLine(y);
-            return y;
-        }
-
-        private void BakeWavelengthPositions()
-        {
-            // Calculate all the X values once as they don't change
-            for (int i = 0; i < Global.SAMPLE_SIZE; i++)
-            {
-                wavelengthPositions[i] = (int)((double)(Global.WAVELENGTHS[i] - Global.WAVELENGTHS[0]) / (double)Global.SPECTROMETER_RANGE * (double)WIDTH) + POS_X;
-                // Console.WriteLine(wavelengthPositions[i]);
-            }
-        }
 
         private void UpdateAllColors()
         {
@@ -175,6 +270,12 @@ namespace SunSpecGraphing
         {
             DrawDataGraph();
             DrawSpectraBar();
+        }
+
+        public void UpdateSpectrometerData(SpectrometerDataHandler newData)
+        {
+            data = newData;
+            MAX_INTENSITY_VALUE = data.GetMaxIntensity();
         }
     }
 }
--- SunSpecGraphing/mainWindow.cs
+++ SunSpecGraphing/mainWindow.cs
@@ -4,6 +4,7 @@ namespace SunSpecGraphing
     {
         private GraphingAPI graph;
         private SpectrometerDataHandler currentData;
+        private System.Windows.Forms.Timer updateTimer;
 
         public mainWindow()
         {
@@ -15,19 +16,25 @@ namespace SunSpecGraphing
             this.MaximizeBox = false;
 
             currentData = new SpectrometerDataHandler("C:\\Users\\pokem\\Desktop\\Share\\AllData\\data.spec");
-            graph = new GraphingAPI(800, 500, 0, 75, this, currentData);
+            graph = new GraphingAPI(600, 400, 100, 75, this, currentData);
+
+            updateTimer = new System.Windows.Forms.Timer();
+            updateTimer.Interval = 10000; // 10 seconds
+            updateTimer.Tick += new EventHandler(UpdateTimer_Tick);
+            updateTimer.Start();
+
         }
+
+        private void UpdateTimer_Tick(object sender, EventArgs e)
+        {
+            //graph.UpdateSpectrometerData();
+            UpdateGraphs();
+        }
+
 
         private void UpdateGraphs()
         {
-            try
-            {
-                graph.UpdateGraph();
-            } 
-            catch
-            {
-
-            }
+            graph.UpdateGraph();
         }
 
         private void toggleGraphColorToolStripMenuItem_Click(object sender, EventArgs e)
